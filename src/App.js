f93aeacc53946ea8e1d59be3ccb0a78924ac6c24
Silas Eduardo <silasn1@live.com>
2023-09-25 07:31:12 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './src/App.css';
+import head_login from './img/head_login.png'; 
 
 function App() {
   return (
-    <div>
-      <header>
-
-      </header>
-    </div>
+    <section class="login">
+      <section class="login-img">
+        <img src={head_login} alt='logo portal'></img>
+      </section>
+      <section class="login-form">
+        <form></form>
+      </section>
+    </section>
   );
 }
 
 export default App;
+ 
